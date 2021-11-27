7dde4919b5e5eff0c0544a3f61c8ba6ab3644a50
Abdallah El Dali <abdallahed19@gmail.com>
2021-11-27 18:12:37 -0500

    fix: added constraints to the book such as quantity must not be null and be higher than 0

--- sql/tables/basket.sql
+++ sql/tables/basket.sql
@@ -2,7 +2,9 @@
 create table basket(
     customer_email varchar(50),
     isbn           int,
-    quantity       int,
+    quantity       int not null,
+
+    check (quantity > 0),
 
     primary key (customer_email, isbn),
     foreign key (customer_email) references customer
