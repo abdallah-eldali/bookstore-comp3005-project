c6fcf7af20176c11be8be252db68f4ccd2949b2f
Abdallah El Dali <abdallahed19@gmail.com>
2021-11-27 17:39:56 -0500

    docs: fixed comments

--- sql/triggers/remove_user.sql
+++ sql/triggers/remove_user.sql
@@ -3,7 +3,7 @@
 --in the parent entity since an entity can only either be a
 --publisher or a user_account
 
---function that removes publisher from entity when a tuple is removed in the publisher table
+--function that removes user from entity when a tuple is removed in the user table
 create or replace function remove_user_in_entity()
     returns trigger
     language plpgsql
@@ -18,7 +18,7 @@ begin
 end;
 $$;
 
---trigger: when a publisher is removed, then remove it from entity as well
+--trigger: when an user is removed, then remove it from entity as well
 create trigger remove_user
 after delete on user_account
 for each row 
