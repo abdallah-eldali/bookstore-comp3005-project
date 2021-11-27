2b23615cd2ca8bc4f46e3c716130d58fcc45c0e5
Abdallah El Dali <abdallahed19@gmail.com>
2021-11-27 16:12:32 -0500

    fix: removed redudant constraints

--- sql/tables/entity.sql
+++ sql/tables/entity.sql
@@ -1,7 +1,6 @@
---TODO: Test this
 create table entity(
     --since email -> name and is also a superkey
-    email varchar(50) unique not null,
+    email varchar(50),
     name  varchar(20) not null,
 
     primary key (email)
