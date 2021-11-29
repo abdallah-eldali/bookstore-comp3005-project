baa138b3eead3c494d33b1d5f7aae75bd3d73d9f
Suhail Mohamed <suhailam2000@gmail.com>
2021-11-28 19:36:59 -0500

    Fixing typo

--- sql/tables/billing.sql
+++ sql/tables/billing.sql
@@ -1,7 +1,7 @@
 create table billing(
     card_number varchar(16),
 
-    -- first check for lengsleth (supposedly can be 8 digits long), second check
+    -- first check length (supposedly can be 8 digits long), second check
     -- is to make sure the card_number only contains digits no letters
     check(length(card_number) >= 8 and card_number ~ '^\d*$'),
     primary key (card_number)
