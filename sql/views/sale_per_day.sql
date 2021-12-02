4172acb20dc4268d202bb401e2c2c9b97a93dcc2
Abdallah El Dali <abdallahed19@gmail.com>
2021-12-02 15:56:46 -0500

    feat: create a materialized view that shows sales per date

---
+++ sql/views/sale_per_day.sql
@@ -0,0 +1,21 @@
+create or replace function sale_on_day(day date)
+	returns decimal(10,2)
+	language plpgsql
+as
+$$
+begin
+
+	return (select sum(book_order.quantity * book.price)
+		    from book_order, book
+		    where book_order.isbn = book.isbn and book_order.order_id in (select order_id
+																		  from order_table
+																		  where order_date = day));
+	
+	
+end;
+$$;
+
+
+create materialized view sale_per_day(day, sales) as 
+	select distinct order_date, sale_on_day(order_date)
+	from order_table;
